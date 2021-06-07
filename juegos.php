87387471eff521e712b0ab12a741019a2f824793
JorgeAV99 <83812907+JorgeAV99@users.noreply.github.com>
2021-06-07 14:25:25 +0200

    barra de busqueda juego

--- juegos.php
+++ juegos.php
@@ -4,8 +4,26 @@ require_once __DIR__.'/includes/config.php';
 
 $tituloPagina = 'Exotic Games Academy - Juegos';
 
-$juegos = \es\fdi\ucm\aw\Juego::obtenerTodosLosJuegos();
-$contenidoPrincipal = obtenerJuegosParaDisplay($juegos);
+
+$contenidoPrincipal =<<<EOS
+
+<div class= "busquedaCSS">
+<form action="juegos.php" method="post">
+	<input type="search" placeholder="Buscar" name="busquedaJuego">
+</form>
+</div>
+
+EOS;
+
+$busqueda = isset($_POST["busquedaJuego"]) ? $_POST["busquedaJuego"] : null;
+
+if ($busqueda != null){
+    $juegos = \es\fdi\ucm\aw\Juego::mostrarJuegos($busqueda);
+} else {
+    $juegos = \es\fdi\ucm\aw\Juego::obtenerTodosLosJuegos();
+}
+
+$contenidoPrincipal.= obtenerJuegosParaDisplay($juegos);
 
 
 require __DIR__.'/includes/comun/layout.php';
