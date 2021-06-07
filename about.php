bda1b48a7fc79f7b942f5f3e857d595f1c16f26d
danielduri <danielduri@gmail.com>
2021-06-07 14:31:57 +0200

    about.php

--- about.php
+++ about.php
@@ -8,7 +8,7 @@ $contenidoPrincipal=<<<EOS
 <h1 class="mainTitle">En Exotic Games Academy queremos convertir jugadores amateurs o avanzados en verdaderos jugadores profesionales de eSports, capaces de competir en las principales ligas nacionales e internacionales.</h1>
 
 <h2 class="contenido">
-Nuestra academia dispone de pollas para muchos de los juegos más a la moda en la actualidad como por ejemplo League of Legends, COD: Warzone, Chess... Contamos con todo tipo de ayudas para que puedas llegar a el nivel más alto como por ejemplo tutoriales, coaching, foros, entradas, consejos de jugadores profesionales, análisis de tús partidas...</h1>
+Nuestra academia dispone de cursos para muchos de los juegos más a la moda en la actualidad como por ejemplo League of Legends, COD: Warzone, Chess... Contamos con todo tipo de ayudas para que puedas llegar a el nivel más alto como por ejemplo tutoriales, coaching, foros, entradas, consejos de jugadores profesionales, análisis de tús partidas...</h1>
 </h2>
 EOS;
 
