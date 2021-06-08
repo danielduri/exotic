6a0635425ecfe2294bf2bd1f6abefe3e69be0ebb
danielduri <danielduri@gmail.com>
2021-06-08 13:06:59 +0200

    cambiar foto Perfil

--- profile.php
+++ profile.php
@@ -9,8 +9,10 @@ $user = \es\fdi\ucm\aw\Usuario::buscaUsuario($_SESSION["username"]);
 
 $contenidoPrincipal=$user->datosUsuario($_SESSION["username"]);
 $contenidoPrincipal.=<<<EOS
-
+<div class="navigationButton">
 <a href = "profileEdit.php" class="submitbtn"> <button> Modifica tus datos </button></a>
+<a href = "fotoPerfil.php" class="submitbtn"> <button> Modifica tu foto de perfil </button></a>
+</div>
 
 EOS;
 
