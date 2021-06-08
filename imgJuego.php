547fb71c47ba95127b5f19ada7f22be01cc2fc93
danielduri <danielduri@gmail.com>
2021-06-09 01:13:10 +0200

    seguridad htmlspecialchars. hacer el resto de formularios

--- imgJuego.php
+++ imgJuego.php
@@ -6,12 +6,17 @@ $tituloPagina = 'Exotic Games Academy - Editar foto de juego';
 $juego = \es\fdi\ucm\aw\Juego::buscarJuegoPorNombre($_GET["juego"]);
 $formulario = new \es\fdi\ucm\aw\FormulariosAdmin\FormularioImgJuego();
 
-$procesamiento = $formulario->gestiona();
-$contenidoPrincipal= <<<EOS
-<div class="login">
-$procesamiento
-</div>
-<a href = "adminJuegos.php" class="submitbtn"> <button> Volver </button></a>
+if(isset($_SESSION["userID"]) && $_SESSION["admin"]){
+    $procesamiento = $formulario->gestiona();
+    $contenidoPrincipal= <<<EOS
+    <div class="login">
+    $procesamiento
+    </div>
+    <a href = "adminJuegos.php" class="submitbtn"> <button> Volver </button></a>
 EOS;
+
+}else{
+    $contenidoPrincipal="<h1>No tiene permiso para acceder a esta página</h1>";
+}
 
 require __DIR__.'/includes/comun/layout.php';
